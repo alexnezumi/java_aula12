1e17cb7161639f62ecb7496db2f985a697299ef2
alex <alexsilva.viana2006@gmail.com>
2024-11-05 21:45:32 -0300

    innitial commit

---
+++ src/main/Main.java
@@ -0,0 +1,15 @@
+
+package main;
+
+import View.Inicio_GUI;
+import View.Splash_GUI;
+
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+    new Splash_GUI().setVisible(true);
+    }
+    
+}
